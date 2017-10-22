529c0de7161868b99d1e00782f107122b932fd6e
Damien Lowe <ashleydamienlowe@gmail.com>
2017-10-22 12:08:11 +1100

    Unit 11, no resetBoard

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,3 @@
-// var cardOne = "Queen";
-// var cardTwo = "Queen";
-// var cardThree = "King";
-// var cardFour = "King";
 
 
 var cards = [
@@ -28,33 +24,51 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var counter = 0;
 
-var checkForMatch = function () {
-
-	if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+var checkForMatch = function() {
+		if (cardsInPlay.length === 2) {
+		if ( cardsInPlay[0] === cardsInPlay[1] ) {
+			counter++;
+			// resetBoard();
+			alert("You've found a match! Your score is " + counter);
+		} else {
+			alert("Sorry, please try again. Your score is " + counter);
+			// resetBoard();
+		} 
 	}
-	};
 };
 
-var flipCard = function (cardId) {
-	
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].suit);
-
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
+	console.log("User flipped a " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
 	checkForMatch();
 };
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
 
-flipCard(0);
-flipCard(2);
+// var resetBoard = function () {
+// 	for (var i = 0; i < card.length; i++) {
+// 		document.getElementsByTagName("img")[i].setAttribute("src", "image/back.png");
+// 	};
+// 	cardsInPlay = [];
+// };
 
+// var resetButton = function () {
+// 	document.getElementById("reset").addEventListener("click", resetBoard);
+// 	counter = 0;
+// }; 
 
+createBoard();
 
 
